ffe9be63ec2b8faf2e5f8e269231ae5cbcf5efa7
Anna Lee <annaleeart@yandex.ru>
2020-06-11 17:13:10 +0300

    Created errorHandler middleware

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { createUser, login } = require('./controllers/users');
 
 const { PORT = 3030 } = process.env;
 const auth = require('./middlewares/auth');
+const { errorHandler } = require('./middlewares/error-handler');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -40,19 +41,14 @@ app.use(limiter);
 app.post('/signup', createUser);
 app.post('/signin', login);
 
-// авторизация
-app.use(auth);
 // роуты, которым авторизация нужна
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+// авторизация
+app.use(auth);
+
 // здесь обрабатываются все ошибки
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    // проверяем статус и выставляем сообщение в зависимости от него
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-});
+app.use(errorHandler);
 
 app.all('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
---
+++ middlewares/error-handler.js
@@ -0,0 +1,16 @@
+
+module.exports.errorHandler = (error, req, res, next) => {
+  const errorCodes = {
+    NotFoundError: 404,
+    CastError: 400,
+    ValidationError: 400,
+    Unauthorized: 401,
+    Forbidden: 403,
+    Conflict: 409,
+  };
+  const errorName = error.name;
+  const { message } = error || 'Неизвестная ошибка';
+  const errorStatus = errorCodes[error.name] || 500;
+  res.status(errorStatus).send({ errorName, message });
+  next();
+};
