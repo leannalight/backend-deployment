4b6d10b05f900e790c56a63defcd5d0c734165ab
Anna Lee <annaleeart@yandex.ru>
2020-06-21 13:17:41 +0300

    Added NODE_ENV !== 'production' instead of NODE_ENV === 'development'

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const { JWT_SECRET, NODE_ENV } = process.env;
 
-module.exports.PrivateKey = NODE_ENV === 'development' ? 'some-dev-secret' : JWT_SECRET;
+module.exports.PrivateKey = NODE_ENV !== 'production' ? JWT_SECRET : 'some-dev-secret';
